e0b3acbd7f6c61487cf6b68428b7672788fb3cf4
Your Name~ <naiumur202163@gmail.com>
2022-02-28 17:20:23 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import { useCallback, useEffect, useState } from 'react';
+import { useDropzone } from 'react-dropzone';
 import './App.css';
-
 function App() {
+  const [images,setImages]=useState([])
+    function handleUpload(){
+      console.log("Uploading Filess......")
+    }
+  const onDrop= useCallback((acceptedFiles,rejectedFiles)=>{
+      console.log("acceptedFiles",acceptedFiles)
+      acceptedFiles.forEach(file=>{
+        const reader= new FileReader()
+        reader.onload=()=>{
+          setImages(prevState=>[...prevState,reader.result])
+        }
+        reader.readAsDataURL(file)
+      })
+      console.log("rejectedFiles",rejectedFiles)
+
+  },[])
+  const {getRootProps,getInputProps,isDragActive}=useDropzone({onDrop,
+    accept: 'image/jpeg,image/png'
+  });
+
+  useEffect(()=>{
+    console.log(images)
+  },[images])
+  console.log(getInputProps(),getRootProps())
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className='dropzone' {...getRootProps()}>
+          <input {...getInputProps} />
+          {isDragActive? "drageActive" :"You can drope your file"}
+        </div>
+        {images.length>0 && <div>
+          {images.map((image,index)=><img className='selected-images' src={image} alt="" /> )}
+          </div>}
+
+
+          {images.length>0 && <button onClick={handleUpload}>Upload </button>}
     </div>
   );
 }
